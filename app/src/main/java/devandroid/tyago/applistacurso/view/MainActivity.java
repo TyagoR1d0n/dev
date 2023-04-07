20d6b096ec071b972c98c4212af50a91eb2779eb
Tyago Santana <worktyago@live.com>
2023-04-06 23:17:04 -0400

    Uso dos metodos get e set

--- app/src/main/java/devandroid/tyago/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/tyago/applistacurso/view/MainActivity.java
@@ -11,15 +11,22 @@ import devandroid.tyago.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
  Pessoa pessoa;
+ Pessoa outraPessoa;
+
+ String dadosPessoa;
+ String dadosOutraPessoa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        outraPessoa = new Pessoa();
 
-        //atribuir dados,conteudos,valores para o objeto
-        //conforme o seu MODELO, TEMPLATE
+
+        //atribuir dados,conteudos,valores para o objeto;
+        //conforme o seu MODELO, TEMPLATE;
 
         pessoa.setPrimeiroNome("Tyago");
         pessoa.setSobreNome("Santana");
@@ -28,6 +35,32 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setCidade("Manaus");
         pessoa.setEstado("Amazonas");
 
-        int parada=0;
+        dadosPessoa += "Nome Pessoa ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += "Sobrenome ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += "Curso Desejado ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += "Telefone para contato ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        outraPessoa.setPrimeiroNome("Joao");
+        outraPessoa.setSobreNome("Sant");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("92991123797");
+        outraPessoa.setCidade("Recife");
+        outraPessoa.setEstado("Pernambuco");
+
+        dadosOutraPessoa += "Nome Pessoa";
+        dadosOutraPessoa +=outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += "Sobrenome";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += "Curso Desejado";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += "Telefone contato";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+int parada = 0;
+
     }
 }
